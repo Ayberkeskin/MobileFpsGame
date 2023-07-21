f3ed11bfc63ee9006f58fadfbf02698019926eb4
Ayberk Keskin <81094165+Ayberkeskin@users.noreply.github.com>
2023-07-22 01:39:13 +0300

    EnemyMovment
    Enemy nin playerı takşo etme kodu yazıldı

--- Assets/Scripts/Character/Enemy/EnemyMovement.cs
+++ Assets/Scripts/Character/Enemy/EnemyMovement.cs
@@ -26,11 +26,21 @@ public class EnemyMovement : MonoBehaviour
     private void FixedUpdate()
     {
         Move();
+        LookTheTargert(_target.position);
     }
 
     private void Move()
     {
         if (_target == null) { return;  }
-        
+
+        _agent.SetDestination(_target.position);
     }
+
+    private void LookTheTargert(Vector3 target)
+    {
+        Vector3 lookPos = new Vector3(target.x, transform.position.y, target.z);
+        transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(lookPos - transform.position),
+            _turnSpeed * Time.deltaTime);
+    }
+
 }
